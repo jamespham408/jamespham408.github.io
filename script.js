c9d89508de00d18aa81dcac5333e0ee98abb9aa2
jamespham408 <jamespham408@gmail.com>
2023-10-31 17:42:25 -0700

    MobileFriendly 04
    fix nav button

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var buttonToDesktop = document.getElementById('toDesktop');
 // Add a click event listener to the button
 buttonToMobile.addEventListener('click', function() {
     // Redirect the user to the specified URL
-    window.location.href = "./mobile.html"; // Replace with your desired URL
+    window.location.href = "mobile.html"; // Replace with your desired URL
 });
 
 // Add a click event listener to the button
 buttonToDesktop.addEventListener('click', function() {
     // Redirect the user to the specified URL
-    window.location.href = "./index.html"; // Replace with your desired URL
+    window.location.href = "index.html"; // Replace with your desired URL
 });

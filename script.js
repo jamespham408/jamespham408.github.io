d7178244c4976e8e62fa5ca2dadff4206cd36fa8
jamespham408 <jamespham408@gmail.com>
2023-10-31 17:37:11 -0700

    MobileFriendly 03
    add mobile version
    add button to navigate between the two

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get the button element by its ID
+var buttonToMobile = document.getElementById('toMobile');
+var buttonToDesktop = document.getElementById('toDesktop');
+
+// Add a click event listener to the button
+buttonToMobile.addEventListener('click', function() {
+    // Redirect the user to the specified URL
+    window.location.href = "./mobile.html"; // Replace with your desired URL
+});
+
+// Add a click event listener to the button
+buttonToDesktop.addEventListener('click', function() {
+    // Redirect the user to the specified URL
+    window.location.href = "./index.html"; // Replace with your desired URL
+});
